79f075bd29bfef81dc0375aadd2dce594ff44502
Moila, Emmanuel <EMoila@fnb.co.za>
2020-05-02 10:54:11 +0200

    initial commit

---
+++ src/main/java/com/example/FruitsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsApplication.class, args);
+	}
+
+}
